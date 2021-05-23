7a90905bd40c669333f422c038a5438daa2a1337
mistico0121 <mrschudeck@uc.cl>
2021-05-22 22:09:15 -0400

    intento fuerza bruta

--- tarea5logica.py
+++ tarea5logica.py
@@ -1,8 +1,13 @@
+import itertools
+
 def notFunction(boolean):
 	if boolean:
 		return False
 	else:
 		return True
+
+def orFunction(a, b):
+	return (a or b)
 
 def ParserDimacs(filename):
 	
@@ -15,7 +20,7 @@ def ParserDimacs(filename):
 	numeroValuaciones = 0
 
 	direcciones = {}
-	valuaciones = {}
+	valuaciones = []
 
 	counter = 0
 
@@ -37,19 +42,61 @@ def ParserDimacs(filename):
 				numeroVariables = int(linearray[2])
 				numeroValuaciones = int(linearray[4])
 
-				for i in numeroVariables:
-					variables[i] = False
+				for i in range(numeroVariables):
+					direcciones[i] = False
 
 				readingInstructions = True
 
 		else:
+			if linearray[0] == '%':
+				break
+
 			listToAdd = []
 
 			for j in linearray:
-				numbah = int(j)
-				if numbah < 0:
+				if j:
+					listToAdd.append(int(j))
+
+
+			valuaciones.append(listToAdd)
 
 	print(f"Numero de variables: {numeroVariables}")
 	print(f"Numero de valuaciones: {numeroValuaciones}")
+
+	print(valuaciones)
+
+	l = [False, True]
+	iterating = list(itertools.product(l, repeat = numeroValuaciones))
+
+
+
+	for combination in iterating:
+		counter = 1
+		for value in combination:
+			direcciones[counter] = value
+			counter += 1
+
+		boolProp = True
+
+		for proposition in valuaciones:
+			toEvaluate = []
+			for value in proposition:
+				if value>0:
+					toEvaluate.append(direcciones(value))
+				elif value<0:
+					toEvaluate.append(notFunction(direcciones(value)))
+
+			boolProp = reduce(orFunction, toEvaluate)
+
+			if not boolProp:
+				break
+
+		if counter == numeroValuaciones:
+			return True
+
+	return False
+
+
+
 
 ParserDimacs("uf20-01.cnf")
